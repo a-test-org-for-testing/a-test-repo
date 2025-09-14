b63fd8f6c84cfcc3ddf56277d2bf1d4c9260e078
AJ Schmidt <ajs4392@gmail.com>
2025-09-13 22:04:39 -0400

    update dynamic pipeline

--- dynamic.sh
+++ dynamic.sh
@@ -9,17 +9,17 @@ cat <<EOF
 steps:
   - command: echo "building \\\$USR_ARCH \\\$USR_PY_VER \\\$USR_CUDA_VER!"
     label: ":construction_worker: build $SUFFIX {{ matrix.arch }} {{ matrix.py_ver}} {{ matrix.cuda_ver }}"
-    key: "build"
+    key: "build-$SUFFIX"
     matrix:
       setup:
         arch:
           - amd64
           - arm64
         py_ver:
-          - "310"
+          - "3.10"
         cuda_ver:
-          - "110"
-          - "120"
+          - "11.0"
+          - "12.0"
     env:
       USR_ARCH: "{{ matrix.arch }}"
       USR_PY_VER: "{{ matrix.py_ver }}"
@@ -28,18 +28,18 @@ steps:
   - command: echo "testing \\\$USR_ARCH \\\$USR_PY_VER \\\$USR_CUDA_VER!"
     label: ":white_check_mark: Test $SUFFIX {{ matrix.arch }} {{ matrix.py_ver}} {{ matrix.cuda_ver }}"
     depends_on:
-      - "build"
+      - "build-$SUFFIX"
     matrix:
       setup:
         arch:
           - amd64
           - arm64
         py_ver:
-          - "310"
-          - "311"
+          - "3.10"
+          - "3.11"
         cuda_ver:
-          - "110"
-          - "120"
+          - "11.0"
+          - "12.0"
     env:
       USR_ARCH: "{{ matrix.arch }}"
       USR_PY_VER: "{{ matrix.py_ver }}"
