0d2c012644fd05e3f2702bb64ba1ec27644f2ec4
AJ Schmidt <ajs4392@gmail.com>
2025-09-23 22:59:20 -0400

    test groups again

--- generate-builds.sh
+++ generate-builds.sh
@@ -7,22 +7,24 @@ SUFFIX="$1"
 
 cat <<EOF
 steps:
-  - command: echo "building \\\$USR_ARCH \\\$USR_PY_VER \\\$USR_CUDA_VER!"
-    group: build
-    label: ":construction_worker: build $SUFFIX {{ matrix.arch }} {{ matrix.py_ver}} {{ matrix.cuda_ver }}"
-    key: "build-$SUFFIX"
-    matrix:
-      setup:
-        arch:
-          - amd64
-          - arm64
-        py_ver:
-          - "3.10"
-        cuda_ver:
-          - "11.0"
-          - "12.0"
-    env:
-      USR_ARCH: "{{ matrix.arch }}"
-      USR_PY_VER: "{{ matrix.py_ver }}"
-      USR_CUDA_VER: "{{ matrix.cuda_ver }}"
+  - group: build
+    steps:
+      - command: echo "building \\\$USR_ARCH \\\$USR_PY_VER \\\$USR_CUDA_VER!"
+        group: build
+        label: ":construction_worker: build $SUFFIX {{ matrix.arch }} {{ matrix.py_ver}} {{ matrix.cuda_ver }}"
+        key: "build-$SUFFIX"
+        matrix:
+          setup:
+            arch:
+              - amd64
+              - arm64
+            py_ver:
+              - "3.10"
+            cuda_ver:
+              - "11.0"
+              - "12.0"
+        env:
+          USR_ARCH: "{{ matrix.arch }}"
+          USR_PY_VER: "{{ matrix.py_ver }}"
+          USR_CUDA_VER: "{{ matrix.cuda_ver }}"
 EOF
--- generate-tests.sh
+++ generate-tests.sh
@@ -7,24 +7,26 @@ SUFFIX="$1"
 
 cat <<EOF
 steps:
-  - command: echo "testing \\\$USR_ARCH \\\$USR_PY_VER \\\$USR_CUDA_VER!"
-    group: tests
-    label: ":white_check_mark: test $SUFFIX {{ matrix.arch }} {{ matrix.py_ver}} {{ matrix.cuda_ver }}"
-    depends_on:
-      - "build-$SUFFIX"
-    matrix:
-      setup:
-        arch:
-          - amd64
-          - arm64
-        py_ver:
-          - "3.10"
-          - "3.11"
-        cuda_ver:
-          - "11.0"
-          - "12.0"
-    env:
-      USR_ARCH: "{{ matrix.arch }}"
-      USR_PY_VER: "{{ matrix.py_ver }}"
-      USR_CUDA_VER: "{{ matrix.cuda_ver }}"
+  - group: test
+    steps:
+      - command: echo "testing \\\$USR_ARCH \\\$USR_PY_VER \\\$USR_CUDA_VER!"
+        group: tests
+        label: ":white_check_mark: test $SUFFIX {{ matrix.arch }} {{ matrix.py_ver}} {{ matrix.cuda_ver }}"
+        depends_on:
+          - "build-$SUFFIX"
+        matrix:
+          setup:
+            arch:
+              - amd64
+              - arm64
+            py_ver:
+              - "3.10"
+              - "3.11"
+            cuda_ver:
+              - "11.0"
+              - "12.0"
+        env:
+          USR_ARCH: "{{ matrix.arch }}"
+          USR_PY_VER: "{{ matrix.py_ver }}"
+          USR_CUDA_VER: "{{ matrix.cuda_ver }}"
 EOF
